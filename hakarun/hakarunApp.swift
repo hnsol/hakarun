3fdf908701d8141624a7e134c7785905e4d9dae2
hann-solo <50252339+hann-solo@users.noreply.github.com>
2021-02-16 15:34:08 +0900

    Initial Commit

---
+++ hakarun/hakarunApp.swift
@@ -0,0 +1,17 @@
+//
+//  hakarunApp.swift
+//  hakarun
+//
+//  Created by masatora on 2021/02/16.
+//
+
+import SwiftUI
+
+@main
+struct hakarunApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
