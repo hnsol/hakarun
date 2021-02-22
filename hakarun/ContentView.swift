0036f22e7ff935ebcad5f827b9ac317adb457e57
hann-solo <50252339+hann-solo@users.noreply.github.com>
2021-02-22 20:07:31 +0900

    大幅に変えようと思うので、その前のコミット

--- hakarun/ContentView.swift
+++ hakarun/ContentView.swift
@@ -54,7 +54,7 @@ func loadText(_ fileName:String) -> String? {
     } catch {
         return nil
     }
-
+    
 }
 
 // キーボードを下げる
@@ -74,104 +74,104 @@ struct TextforEditor {
 
 
 struct ContentView: View {
-//    @State var theText: String = ""
-//    @State var strApnd: String = ""
-    @State var theText: String = loadText("sample.txt") ?? ""
+    //    @State var theText: String = ""
+    //    @State var strApnd: String = ""
+    var fileNam: String = "hakarun_data.txt"
+    @State var theText: String = loadText("hakarun_data.txt") ?? "yyyy-MM-dd xx.x"
     @State var strTemp: String = "36.1"
     @State var theDate = Date()
-
+    
     // 日付書式
     var dateFormat1: DateFormatter {
         let df = DateFormatter()
         df.dateFormat = "yyyy-MM-dd"
         return df
     }
-
+    
     var body: some View {
         
-        VStack {
-            NavigationView {
-                TextEditor(text: $theText)
-                    .lineSpacing(10)
-                    .border(Color.gray)
-                    .padding([.leading, .bottom, .trailing])
-                    .navigationTitle("はかるん")
-                    .toolbar {
-                        // 読み込みボタン
-//                        ToolbarItem(placement: .navigationBarTrailing) {
-//                            Button {
-//                                if let data = loadText("sample.txt") {
-//                                    theText = data
-//                                }
-//                            } label: {
-//                                Text("読み込み")
-//                            }
-//                        }
-                        // 保存ボタン
-                        ToolbarItem(placement: .navigationBarTrailing) {
-                            Button {
-                                UIApplication.shared.endEditing()
-                                saveText(theText, "sample.txt")
-                            } label: {
-                                Text("保存")
-                            }
-                        }
-                        // 消去ボタン
-                        ToolbarItem(placement: .navigationBarTrailing) {
-                            Button {
-                                theText = ""
-                            } label: {
-                                Text("消去")
-                            }
-                        }
-                    }
-            }
-//            .frame(height: 400.0)
+        ZStack () {
+            // 背景のタップでキーボードを下げる
+            Color.white
+                .onTapGesture {
+                    UIApplication.shared.endEditing()
+                }
             
-            HStack {
-                DatePicker("日付", selection: $theDate, displayedComponents: .date)
-                    .frame(width: 160.0)
-                    .environment(\.locale, Locale(identifier: "ja_JP"))
-                    .padding(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
+            VStack {
+                NavigationView {
+                    TextEditor(text: $theText)
+                        .lineSpacing(10)
+                        .border(Color.gray)
+                        .padding([.leading, .bottom, .trailing])
+                        .navigationTitle("はかるん")
+                        .toolbar {
+                            // 保存ボタン
+                            ToolbarItem(placement: .navigationBarTrailing) {
+                                Button {
+                                    UIApplication.shared.endEditing()
+                                    saveText(theText, fileNam)
+                                } label: {
+                                    Text("保存")
+                                }
+                            }
+                            // 消去ボタン
+                            ToolbarItem(placement: .navigationBarTrailing) {
+                                Button {
+                                    theText = ""
+                                } label: {
+                                    Text("消去")
+                                }
+                            }
+                        }
+                }
+                //            .frame(height: 400.0)
                 
                 HStack {
-                    Label("体温", systemImage: "thermometer")
-                    TextField("体温", text: $strTemp)
+                    DatePicker("日付", selection: $theDate, displayedComponents: .date)
+                        .frame(width: 160.0)
+                        .environment(\.locale, Locale(identifier: "ja_JP"))
                         .padding(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
-                        .frame(width: 80.0)
-                        .border(/*@START_MENU_TOKEN@*/Color.gray/*@END_MENU_TOKEN@*/, width: /*@START_MENU_TOKEN@*/1/*@END_MENU_TOKEN@*/)
-                        .foregroundColor(/*@START_MENU_TOKEN@*/.blue/*@END_MENU_TOKEN@*/)
-                        .keyboardType(/*@START_MENU_TOKEN@*/.decimalPad/*@END_MENU_TOKEN@*/)
-                        .multilineTextAlignment(/*@START_MENU_TOKEN@*/.trailing/*@END_MENU_TOKEN@*/)
-                }
-            }
-            .padding(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
-            
-            HStack {
-                Text(dateFormat1.string(from: theDate) + " " + strTemp)
-                Button(action: {
-                    //                theText = theText + "\n" + dateFormat1.string(from: theDate) + " " + strTemp
-                    let app:String = dateFormat1.string(from: theDate) + " " + strTemp
-                    let arr:[String] = theText.components(separatedBy: "\n")
                     
-                    if arr[0] != app {
-                        theText = app + "\n" + theText
-//                        theText = dateFormat1.string(from: theDate) + " " + strTemp + "\n" + theText
+                    HStack {
+                        Label("", systemImage: "thermometer")
+                        TextField("体温", text: $strTemp)
+                            .padding(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
+                            .frame(width: 80.0)
+                            .border(/*@START_MENU_TOKEN@*/Color.gray/*@END_MENU_TOKEN@*/, width: /*@START_MENU_TOKEN@*/1/*@END_MENU_TOKEN@*/)
+                            .foregroundColor(/*@START_MENU_TOKEN@*/.blue/*@END_MENU_TOKEN@*/)
+                            .keyboardType(/*@START_MENU_TOKEN@*/.decimalPad/*@END_MENU_TOKEN@*/)
+                            .multilineTextAlignment(/*@START_MENU_TOKEN@*/.leading/*@END_MENU_TOKEN@*/)
                     }
-                    
-                }) {
-                    Text("←を上の枠に追記")
                 }
+                .padding(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
+                
+                HStack {
+                    Text(dateFormat1.string(from: theDate) + " " + strTemp)
+                    Button(action: {
+                        //                theText = theText + "\n" + dateFormat1.string(from: theDate) + " " + strTemp
+                        let app:String = dateFormat1.string(from: theDate) + " " + strTemp
+                        let arr:[String] = theText.components(separatedBy: "\n")
+                        
+                        if arr[0] != app {
+                            theText = app + "\n" + theText
+                            //                        theText = dateFormat1.string(from: theDate) + " " + strTemp + "\n" + theText
+                            UIApplication.shared.endEditing()
+                            saveText(theText, fileNam)
+                        }
+                        
+                    }) {
+                        Text("←を上の枠に追記")
+                    }
+                }
+                .padding(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
             }
-            .padding(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
+            
         }
-        
     }
-    
     
 }
 
-        
+
 
 
 struct ContentView_Previews: PreviewProvider {
